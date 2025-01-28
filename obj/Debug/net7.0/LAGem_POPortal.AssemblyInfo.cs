b559958551abf6a587b7d99df2012a650b378d28
Helder <hribeiro@gkginc.com>
2025-01-28 14:54:10 -0500

    Update popup

--- obj/Debug/net7.0/LAGem_POPortal.AssemblyInfo.cs
+++ obj/Debug/net7.0/LAGem_POPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LAGem_POPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c32b7ad589c4d45d064736ef48e6ca14aa1614c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+687bc5dc1dd6ca2d2288e53b9b54e2661f03c37d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LAGem_POPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LAGem_POPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
